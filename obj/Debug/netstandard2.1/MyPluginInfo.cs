f8a897fd6d9a6113fc173d08956f3d518383c342
Lucas11 <63660708+lucas11222@users.noreply.github.com>
2026-09-07 12:22:18 +0200

    Initial commit

---
+++ obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,8 @@
+namespace MultliplayerGoesBRRRRRR;
+
+internal static class MyPluginInfo
+{
+    public const string PLUGIN_GUID = "dev.lucas11.mgb";
+    public const string PLUGIN_NAME = "Multliplayer goes BRRRR";
+    public const string PLUGIN_VERSION = "0.0.1";
+}
---
+++ src/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using Steamworks;
+using UnityEngine;
+
+namespace MultliplayerGoesBRRRRRR;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger { get; private set; } = null!;
+
+    public SteamId SteamID { get; private set; }
+
+    private void Awake()
+    {
+        SteamClient.Init(1229490, true);
+
+        SteamID = SteamClient.SteamId;
+        
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded with SteamID {SteamID}");
+    }
+    
+}
