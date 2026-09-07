08d323d45a3b1442803f5b86da3eac9ca472ed68
Lucas11 <63660708+lucas11222@users.noreply.github.com>
2026-09-07 13:31:22 +0200

    delete thingys we dont need

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace MultliplayerGoesBRRRRRR;
-
-internal static class MyPluginInfo
-{
-    public const string PLUGIN_GUID = "dev.lucas11.mgb";
-    public const string PLUGIN_NAME = "Multliplayer goes BRRRR";
-    public const string PLUGIN_VERSION = "0.0.1";
-}
