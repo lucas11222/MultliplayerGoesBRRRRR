c16ce70707abd86c64ce5501ac4ed9d9bcf79b56
Lucas11 <63660708+lucas11222@users.noreply.github.com>
2026-09-07 14:24:31 +0200

    lobbies!

---
+++ src/Networking/NetworkManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using BepInEx.Logging;
+using Steamworks;
+using Steamworks.Data;
+
+namespace MultliplayerGoesBRRRRRR.Networking
+{
+    public class NetworkManager
+    {
+        public Lobby CurrentLobby;
+
+        public async Task CreateLobby()
+        {
+            Lobby? lobby = await SteamMatchmaking.CreateLobbyAsync(2);
+
+            if (lobby.HasValue)
+            {
+                CurrentLobby = lobby.Value;
+
+                CurrentLobby.SetData("LobbyName", "name");
+                Plugin.Logger.LogInfo(CurrentLobby.Id.ToString());
+            }
+        }
+
+        public async Task JoinLobby(ulong lobbyId)
+        {
+            Lobby? lobby = await SteamMatchmaking.JoinLobbyAsync(lobbyId);
+
+            if (lobby.HasValue)
+            {
+                CurrentLobby = lobby.Value;
+
+                Plugin.Logger.LogInfo(CurrentLobby.GetData("LobbyName"));
+            }
+        }
+    }
+}
--- src/Plugin.cs
+++ src/Plugin.cs
@@ -2,13 +2,15 @@
 using BepInEx.Logging;
 using Steamworks;
 using UnityEngine;
-
+using MultliplayerGoesBRRRRRR.Networking;
 namespace MultliplayerGoesBRRRRRR;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger { get; private set; } = null!;
+
+    public readonly NetworkManager networkManager = new();
 
     public SteamId SteamID { get; private set; }
 
@@ -20,6 +22,11 @@ public class Plugin : BaseUnityPlugin
         
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded with SteamID {SteamID}");
+        networkManager.JoinLobby(109775244839209818);
     }
-    
+
+    private void OnApplicationQuit()
+    {
+        SteamClient.Shutdown();
+    }
 }
